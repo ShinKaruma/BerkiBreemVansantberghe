5945b07db2506fe8b5ecc49e45848ee28f6b0d12
Redwan <redwanberki0@gmail.com>
2021-04-09 06:48:36 +0200

    création fichier de test

---
+++ tests/testModificationBiens.php
@@ -0,0 +1,10 @@
+<?php
+include_once '../modeles/mesFonctionsAccesAuxDonnees.php';
+
+$lePdo = connexionBDD();
+
+var_dump($lePdo);
+
+$bdd->exec('UPDATE modificationBien(IDb, Type, Desc, Jardin, Taille, NbPiece, Prix, Ville, Adresse) VALUES(2, 1, \'jsp\', \'non\', \'65m²\', 3, 125000, \'Lille\', \'5 rue oui\')');
+
+var_dump($bdd);
